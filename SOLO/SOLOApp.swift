c7e4627f7e8003d722ef4dd16e9f411067d9dc60
Sakshi <sakshij@zeta.tech>
2023-10-30 18:13:26 +0530

    Initial Commit

---
+++ SOLO/SOLOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLOApp.swift
+//  SOLO
+//
+//  Created by sakshi.j on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SOLOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
